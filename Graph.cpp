0c9da8107cc858265d9f85e58fa30a875499a3e0
Ian Rodrigues dos Reis Paixão <36201596+IanPaixao@users.noreply.github.com>
2018-06-18 09:05:40 -0300

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -130,12 +130,12 @@ void GraphDFS(int start, int end)
 int main()
 {
 Graph g(5);
+g.addEdge(0, 1);
 g.addEdge(1, 2);
 g.addEdge(2, 3);
 g.addEdge(3, 4);
-g.addEdge(4, 5);
-g.addEdge(5, 1);
-g.DFS(1, 5);
+g.addEdge(4, 0);
+g.DFS(0, 4);
 
 return 0;
 }
