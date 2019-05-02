2db719e43a3ac24496013349c1a1cf5c3f0d62f0
Ian Rodrigues dos Reis Paixão <36201596+IanPaixao@users.noreply.github.com>
2019-05-02 13:32:28 -0300

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,29 +1,30 @@
-#include iostream
-#include stack
-#include string
+
+#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
- Abstraction of a graph
+// Abstraction of a graph
 
 class Graph
 {
-private
-	int n;							 Number of vertices
-	int adj;						 Adjacency matrix
-	int length;                      Length of the Edge 
-public
-	Graph(int size);				 Constructor
-	~Graph();						 Destructor
-	void addEdge(int x, int y); 	 Add an edge the the graph
-	bool isConnected(int x, int y);	 Check if two vertices are connected
-	void DFS(int start, int end);	 Depth first search
+private:
+	int n;							// Number of vertices
+	int** adj;						// Adjacency matrix
+	int length;                     // Length of the Edge
+public:
+	Graph(int size);				// Constructor
+	~Graph();						// Destructor
+	void addEdge(int x, int y, int z); 	// Add an edge the the graph
+	bool isConnected(int x, int y);	// Check if two vertices are connected
+	void DFS(int start, int end);	// Depth first search
 };
 
- Constructor
+// Constructor
 
-GraphGraph(int size)
+Graph::Graph(int size)
 {
-	if (size  2)				 The graph must have at least 2 vertices
+	if (size < 2)				// The graph must have at least 2 vertices
 	{
 		n = 2;
 	}
@@ -32,100 +33,101 @@ GraphGraph(int size)
 		n = size;
 	}
 
-	adj = new int[n];			 Allocate memory for adjacency matrix
+	adj = new int*[n];			// Allocate memory for adjacency matrix
 
-	for (int i = 0; in; ++i)
+	for (int i = 0; i<n; ++i)
 	{
-		adj[i] = new int[n];	 Allocate memory for adjacency matrix
+		adj[i] = new int[n];	// Allocate memory for adjacency matrix
 	}
 
-	for (int i = 0; in; ++i)
+	for (int i = 0; i<n; ++i)
 	{
-		for (int j = 0; jn; ++j)
+		for (int j = 0; j<n; ++j)
 		{
-			adj[i][j] = 0;		 Initialize the vertices to 0
+			adj[i][j] = -1;		// Initialize the vertices to 0
 		}
 	}
 }
 
- Destructor
+// Destructor
 
-Graph~Graph()
+Graph::~Graph()
 {
-	for (int i = 0; in; ++i)
+	for (int i = 0; i<n; ++i)
 	{
 		delete[] adj[i];
 	}
 	delete[] adj;
 }
 
- Add an edge to the graph
+// Add an edge to the graph
 
-void GraphaddEdge(int x, int y)
+void Graph::addEdge(int x, int y, int z)
 {
-	adj[x][y] = adj[y][x] = 1;
+	adj[x][y] = adj[y][x] = z;
 }
 
- Check if two vertices are connected
+// Check if two vertices are connected
 
-bool GraphisConnected(int x, int y)
+bool Graph::isConnected(int x, int y)
 {
-
-	return ((adj[x][y] == 1) && (adj[y][x] == 1));
+	//return (adj[x - 1][y - 1] == 1);
+	return ((adj[x][y] != -1) && (adj[y][x] != -1));
 }
 
- Depth first search
+// Depth first search
 
-void GraphDFS(int start, int end)
+void Graph::DFS(int start, int end)
 {
 
-	stack int s;	 Create a stack of ints
-	bool visited = new bool[n];	 Boolean array to represent which vertices we've visited
+	stack <int> s;	// Create a stack of ints
+	bool* visited = new bool[n];	// Boolean array to represent which vertices we've visited
+	long*  lengths = new long[n];
 
-	for (int i = 0; i  n; ++i)	 Initialize all elements of visited to false
+	for (int i = 0; i < n; ++i)	// Initialize all elements of visited to false
 	{
 		visited[i] = false;
 		lengths[i] = 0;
 	}
 
-	s.push(start);	 Push the vertex onto the stack
-	visited[start] = true;	 Mark this node as visited
+	s.push(start);	// Push the vertex onto the stack
+	visited[start] = true;	// Mark this node as visited
 
-	if (start == end)	 End the function if we're at our destination
+	if (start == end)	// End the function if we're at our destination
 	{
 		return;
 	}
 
-	cout  Depth-first search starting from point   start  endl;
+	//cout << "Depth-first search starting from point: " << start << endl;
 
-	while (!s.empty())	 While the stack is not empty
+	while (!s.empty())	// While the stack is not empty
 	{
-		int k = s.top();	 Save the top element before popping it off
+		int k = s.top();	// Save the top element before popping it off
 		s.pop();
 
-		if (k == end)	 Break out of the loop if we've reached our destination
+		if (k == end)	// Break out of the loop if we've reached our destination
 		{
 			break;
 		}
 
-		cout  k   ;
+		//cout << k << " ";
 
-		for (int i = 0; i  n; i++)	 For all adjacent vertices
+		for (int i = 0; i < n; i++)	// For all adjacent vertices
 		{
-			if (isConnected(k, i) && !visited[i])	 If they're connected and we haven't visited it yet
+			if (isConnected(k, i) && !visited[i])	// If they're connected and we haven't visited it yet
 			{
-				s.push(i);	 Add it to our stack, and mark that we've visited it
+				s.push(i);	// Add it to our stack, and mark that we've visited it
 				visited[i] = true;
-
+				lengths[i] = lengths[k] + adj[k][i];
 			}
 		}
 	}
-	cout  end  endl;
-	cout  lengths[end]  endl;
-	delete[] visited;	 De-allocate the visited array								
+	//cout << end << endl;
+	cout << lengths[end] << " ";
+	delete[] visited;	// De-allocate the visited array
 }
 
- Main function
+ //Main function
 
 int main()
 {
